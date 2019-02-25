83921a8726bbc412f2f379c75cc5fdf1f0f9db83
Graham Ritter <gmr50@georgetown.edu>
2019-02-25 16:39:39 -0500

    working api validation, final cleanup

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # robo-advisor
 
-Need to install the python-dotenv package to your environment
+Hello! Thanks for using my robo advisors. In order for the software to work, a few requirements need to be met:
+	+The following environments should be installed in your environment: json, os, requests, dotenv, datetime, pandas, sys, csv
+	+A data directory on level up from the directory the python script is in (/data/)
+	+The digital_currency_list.csv must be in the same directory as the python script. It can be found at https://www.alphavantage.co/digital_currency_list/
+		It must be named 'digital_currency_list.csv'
+
+Despite the alphavantage.co claiming "The digital/crypto currency of your choice. It can be any of the currencies in the digital currency list.", that is not true and some of the cryptocurrencies will not work! For testing purposes, keep with well known currencies such as BTC, ETH, LTC.
--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -19,55 +19,65 @@ load_dotenv()
 
 #https://stackoverflow.com/questions/19486369/extract-csv-file-specific-columns-to-list-in-python
 colnames = ['code', 'name']
-filename = "digital_currency_list.csv"
-data = pd.read_csv(filename,names=colnames)
+
+try:	
+	filename = "digital_currency_list.csv"
+	data = pd.read_csv(filename,names=colnames)
+except:
+	sys.exit("The digital_currency_list.csv file is not in the same directory as this script. Please download from https://www.alphavantage.co/digital_currency_list/ and try again.")
 
 currency_symbols = data.code.tolist()
 currency_names = data.name.tolist()
 
+print("Welcome to Graham's Robo Adivsor! This python script will help you choose a cryptocurrency stock to pick.")
+
 while True:
 	try:
-		print("What symbol would you like to check? Enter 'S' to see the accepted symbols!")
-		request_symbol = str(input())
-		#https://stackoverflow.com/questions/9257094/how-to-change-a-string-into-uppercase
-		#takes user input to upper case 
-		request_symbol = request_symbol.upper()
-		if(request_symbol == 'S'):
-			for symbol in currency_symbols: print(" +" + symbol)
-				
+		while True:
+			try:
+				print("What symbol would you like to check? Enter 'S' to see the accepted symbols!")
+				request_symbol = str(input())
+				#https://stackoverflow.com/questions/9257094/how-to-change-a-string-into-uppercase
+				#takes user input to upper case 
+				request_symbol = request_symbol.upper()
+				if(request_symbol == 'S'):
+					for symbol in currency_symbols: print(" +" + symbol)
+						
 
+					raise Exception
+
+				elif(request_symbol not in currency_symbols):
+					print("Not in the accepted cryptocurrencies symbol list, please try again. ")
+					raise Exception 
+				break
+			except:
+				pass
+
+
+		request_url = "https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=" + request_symbol + "&market=USD&apikey=" + "&apikey="
+		request_url = request_url + str(API_KEY)
+
+
+
+		#https://stackoverflow.com/questions/543309/programmatically-stop-execution-of-python-script/543375
+		try:
+			response = requests.get(request_url)
+			parsed_response = json.loads(response.text)
+		except:
+			sys.exit("Something went wrong with the API! Check your internet connection and try again!")
+
+
+		#some of the symbols listed in the digital_currency_list.csv don't work. 
+		if 'Error Message' in parsed_response:
 			raise Exception
 
-		elif(request_symbol not in currency_symbols):
-			print("Not in the accepted cryptocurrencies symbol list, please try again. ")
-			raise Exception 
 		break
+
+
 	except:
+
+		print("Sorry, the API does not have data on that cryptocurrency. Please try again.")
 		pass
-
-
-request_url = "https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=" + request_symbol + "&market=USD&apikey=" + "&apikey="
-request_url = request_url + str(API_KEY)
-
-
-
-
-
-
-
-
-# or could do
-# request_url = f"url={API_KEY}"
-#https://stackoverflow.com/questions/543309/programmatically-stop-execution-of-python-script/543375
-try:
-	response = requests.get(request_url)
-	parsed_response = json.loads(response.text)
-except:
-	sys.exit("Something went wrong with the API! Check your internet connection and try again!")
-
-
-
-
 
 
 #output
@@ -83,6 +93,7 @@ print("Request Sent: " + current_time)
 print("Latest Refreshed: " + parsed_response["Meta Data"]["6. Last Refreshed"])
 
 print("*******************************************")
+
 
 
 #adapted from Prof Rossetti's solution
@@ -213,16 +224,6 @@ else:
 
 print("*******************************************")
 print("*******************************************")
-#csvwriter = csv.writer(parsed_response)
-
-
-
-#for parsed in parsed_response:
-
-
-#      csvwriter.writerow(parsed.text())
-
-#data_csv.close()
 
 
 
