2eefdebdd961bf907481a58c7d851bf1b1d594f3
Graham Ritter <gmr50@georgetown.edu>
2019-02-25 14:48:26 -0500

    working csv writing

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -123,13 +123,14 @@ latest_volume = "{0:.2f}".format(float(latest_volume))
 high_price_list = []
 low_price_list = []
 
+#gets recent high and low
 counter = 0
 for dates in list_keys:
 	if(counter < 100):
 		high_price_list.append(parsed_response["Time Series (Digital Currency Daily)"][dates]["2a. high (USD)"])
 		low_price_list.append(parsed_response["Time Series (Digital Currency Daily)"][dates]["3a. low (USD)"])
 		counter = counter+1
-		print(counter)
+
 
 recent_high_price = max(high_price_list)
 recent_high_price = dollar_format(float(recent_high_price))
@@ -157,12 +158,26 @@ print("Recent Low Price: " + str(recent_low_price))
 #http://blog.appliedinformaticsinc.com/how-to-parse-and-convert-json-to-csv-using-python/
 # open a file for writing
 
+counter = 0
+#https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/notes/python/modules/csv.md
+with open('data/prices.csv', 'w') as csv_file:
+	writer = csv.DictWriter(csv_file, fieldnames=["timestamp", "open", "high", "low", "close", "volume", "market cap"])
+	writer.writeheader() 
+	for dates in list_keys:
+		if(counter < 100):
+			counter = counter + 1
+			writer.writerow({"timestamp": dates, 
+			"open": parsed_response["Time Series (Digital Currency Daily)"][dates]["1a. open (USD)"],
+			"high": parsed_response["Time Series (Digital Currency Daily)"][dates]["2a. high (USD)"],
+			"low": parsed_response["Time Series (Digital Currency Daily)"][dates]["3a. low (USD)"],
+			"close": parsed_response["Time Series (Digital Currency Daily)"][dates]["4a. close (USD)"],
+			"volume": parsed_response["Time Series (Digital Currency Daily)"][dates]["5. volume"],
+			"market cap": parsed_response["Time Series (Digital Currency Daily)"][dates]["6. market cap (USD)"]})
 
-#data_csv = open('data/prices.csv', 'w')
-
+    # uses fieldnames set above
 # create the csv writer object
 
-parsed_response = parsed_response["Time Series (Digital Currency Daily)"]
+
 print("****************")
 #print(parsed_response)
 print("*****************")
